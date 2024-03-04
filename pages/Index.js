6e68d01a0dd4354a582c4b727d892297c1a8d0ad
isac7722 <isac7722@gmail.com>
2024-03-04 14:57:43 +0900

    socialAPI

--- pages/Index.js
+++ pages/Index.js
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import DashBoard from "./DashBoard";
+import { SocialLogin } from "./auth/SocialLogin";
+import TestPage, { LoginScreen } from "./auth/LoginScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,7 +12,17 @@ const Index = () => {
             <Tab.Screen
                 name="DashBoard"
                 component={DashBoard}
-        />
+            />
+
+            <Tab.Screen
+                name="Social Login"
+                component={SocialLogin}
+            />
+
+            <Tab.Screen
+                name="Test"
+                component={TestPage}
+            />
         </Tab.Navigator>
     )
 }
