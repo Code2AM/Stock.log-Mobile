5b944fdaf6a41fbd840dc1d6fda6bf105a825b3b
isac7722 <isac7722@gmail.com>
2024-03-08 15:53:20 +0900

    Feat finished notes Front

--- screens/notes/NoteEditorScreen.js
+++ screens/notes/NoteEditorScreen.js
@@ -1,21 +1,110 @@
-import { Button, Input, NativeBaseProvider, TextArea } from "native-base";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Button, Input, NativeBaseProvider, TextArea, useToast } from "native-base";
+import { useEffect, useState } from "react";
+import { deleteNoteRequest, updateNoteRequest } from "../../api/notes/NotesAPI";
+import { useStore } from "zustand";
+import { useNotes } from "../../zustand/notes/useNotes";
 
 const NoteEditorScreen = () => {
 
-    
+    const { fetchAllNotes } = useStore(useNotes);
+
+    const [noteName, setNoteName] = useState("");
+    const [noteContents, setNoteContents] = useState("");
+
+    const route = useRoute(); // 이 부분이 누락되었을 수 있음
+    const { item } = route.params;
+
+    const navigation = useNavigation();
+    const toast = useToast();
+
+    console.log(item)
+
+    useEffect(() => {
+        if (item) {
+            setNoteName(item.noteName);
+            setNoteContents(item.noteContents);
+        }
+    }, [item])
+
+    const handleUpdate = async () => {
+
+        console.log("handleUpdate")
+
+        data = {
+            noteId: item.noteId,
+            noteName: noteName,
+            noteContents: noteContents,
+            noteDate: item.noteDate,
+            noteStatus: item.noteStatus,
+            userId: item.userId,
+        }
+
+        const result = await updateNoteRequest(data);
+        console.log(result)
+
+        fetchAllNotes();
+
+        toast.show({
+            title: "수정 성공",
+            duration: 1500,
+            placement: "top",
+            avoidKeyboard: true,
+          })
+
+        navigation.navigate("NotesScreen");
+    }
+
+    const handleDelete = async () => {
+      
+        console.log("handleUpdate")
+
+        data = {
+            noteId: item.noteId,
+            noteName: noteName,
+            noteContents: noteContents,
+            noteDate: item.noteDate,
+            noteStatus: item.noteStatus,
+            userId: item.userId,
+        }
+
+        const result = await deleteNoteRequest(data);
+        console.log(result)
+
+        fetchAllNotes();
+
+        toast.show({
+            title: "삭제 성공",
+            duration: 1500,
+            placement: "top",
+            avoidKeyboard: true,
+          })
+
+        navigation.navigate("NotesScreen");
+
+    }
 
 
-    return(
+    return (
         <NativeBaseProvider>
             <Input
                 placeholder="제목을 입력해주세요"
+                value={noteName}
+                onChangeText={setNoteName}
             />
 
             <TextArea
                 placeholder="내용을 입력해주세요"
+                value={noteContents}
+                onChangeText={setNoteContents}
             />
 
-            <Button>수정</Button>
+            <Button
+                onPress={handleUpdate}
+            >수정</Button>
+            <Button
+                onPress={handleDelete}
+            >삭제</Button>
         </NativeBaseProvider>
     )
 }
