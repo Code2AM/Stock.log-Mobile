64863e2bce53e415e28982f3e0394f426655a4a1
hiimparkjiho <jiho2689@gmail.com>
2024-03-11 20:41:44 +0900

    fix: labels, notes 연동

--- screens/notes/NewNoteScreen.js
+++ screens/notes/NewNoteScreen.js
@@ -1,72 +1,85 @@
-import { Button, Input, NativeBaseProvider, TextArea, useToast } from "native-base";
-import { useState } from "react";
+import { Box, Button, Center, Checkbox, FlatList, Input, NativeBaseProvider, Select, TextArea, useToast } from "native-base";
+import { useEffect, useState } from "react";
 import { newNoteRequest, notesRequest } from "../../api/notes/NotesAPI";
 import { useNavigation } from "@react-navigation/native";
 import { useStore } from "zustand";
 import { useNotes } from "../../zustand/notes/useNotes";
+import useLabels from "../../zustand/labels/useLabels";
+import LabelsSelectItem from "../../components/labels/LabelsSelectItem";
 
 
 
  
 export const NewNoteScreen = () => {
-
     const { fetchAllNotes } = useStore(useNotes);
-
-    const [ noteName , setNoteName ] = useState("");
-    const [ noteContents , setNoteContents ] = useState("");
-
+    const { labels, fetchAllLabels } = useStore(useLabels);
+  
+    useEffect(() => {
+      fetchAllLabels();
+    }, []);
+  
+    const [noteName, setNoteName] = useState("");
+    const [noteContents, setNoteContents] = useState("");
+    const [selectedLabel, setSelectedLabel] = useState(null); // 선택된 라벨 상태 추가
+  
     const navigation = useNavigation();
     const toast = useToast();
-
-
+  
     const handleNewNote = async () => {
-
-        const data = {
-            noteName : noteName,
-            noteContents : noteContents
-        }
-      
-        const result = await newNoteRequest(data);
-
-        // /* 사용자의 Notes들을 불러옴 */
-        // const fetchedNotes = await notesRequest();
-        // setNotes(fetchedNotes);
-
-        fetchAllNotes();
-        
-        
-        toast.show({
-            title: "등록 성공",
-            duration: 1500,
-            placement: "top",
-            avoidKeyboard: true,
-          })
-
-        navigation.navigate("Notes",{
-            screen: "NotesScreen",
-        })
+      const selectedLabelValue = labels.find(label => label.labelsId === selectedLabel); // 선택된 라벨의 객체 가져오기
+      const data = {
+        noteName: noteName,
+        noteContents: noteContents,
+        labelsDTO: selectedLabelValue // 선택된 라벨의 객체를 data에 추가
+      }
+  
+      const result = await newNoteRequest(data);
+  
+      fetchAllNotes();
+  
+      toast.show({
+        title: "등록 성공",
+        duration: 1500,
+        placement: "top",
+        avoidKeyboard: true,
+      });
+  
+      navigation.navigate("Notes", {
+        screen: "NotesScreen",
+      });
     }
   
-
     return (
-        <NativeBaseProvider>
+      <NativeBaseProvider>
         <Input
-            placeholder="제목을 입력해주세요" 
-            type={"noteName"}
-            value={noteName}
-            onChangeText={setNoteName}
+          placeholder="제목을 입력해주세요"
+          type={"noteName"}
+          value={noteName}
+          onChangeText={setNoteName}
         />
-
+  
         <TextArea
-            placeholder="내용을 입력해주세요"
-            type={"noteContents"}
-            value={noteContents}
-            onChangeText={setNoteContents}
+          placeholder="내용을 입력해주세요"
+          type={"noteContents"}
+          value={noteContents}
+          onChangeText={setNoteContents}
         />
-
-        <Button
-        onPress={handleNewNote}
-        >저장</Button>
-    </NativeBaseProvider>
-    )
-}
+  
+        <Box maxW="300">
+          <Select
+            minWidth="200"
+            accessibilityLabel="Choose Service"
+            placeholder="라벨 선택"
+            value={selectedLabel} // 선택된 라벨 값 설정
+            onValueChange={(itemValue) => setSelectedLabel(itemValue)} // 선택된 라벨 값 변경 시 상태 업데이트
+          >
+            {labels.map((label) => (
+              <Select.Item key={label.labelsId} label={label.labelsTitle} value={label.labelsId} />
+            ))}
+          </Select>
+        </Box>
+  
+        <Button onPress={handleNewNote}>저장</Button>
+      </NativeBaseProvider>
+    );
+  }
--- screens/notes/NoteEditorScreen.js
+++ screens/notes/NoteEditorScreen.js
@@ -1,111 +1,118 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Button, Input, NativeBaseProvider, TextArea, useToast } from "native-base";
+import { Box, Button, Input, NativeBaseProvider, Select, TextArea, useToast } from "native-base";
 import { useEffect, useState } from "react";
 import { deleteNoteRequest, updateNoteRequest } from "../../api/notes/NotesAPI";
 import { useStore } from "zustand";
 import { useNotes } from "../../zustand/notes/useNotes";
+import useLabels from "../../zustand/labels/useLabels";
 
 const NoteEditorScreen = () => {
-
     const { fetchAllNotes } = useStore(useNotes);
-
+  
     const [noteName, setNoteName] = useState("");
     const [noteContents, setNoteContents] = useState("");
-
-    const route = useRoute(); // 이 부분이 누락되었을 수 있음
+    const { labels, fetchAllLabels } = useStore(useLabels);
+    const [selectedLabel, setSelectedLabel] = useState(null); // 선택된 라벨 상태 추가
+  
+    const route = useRoute();
     const { item } = route.params;
-
+  
     const navigation = useNavigation();
     const toast = useToast();
-
-    console.log(item)
-
+  
     useEffect(() => {
-        if (item) {
-            setNoteName(item.noteName);
-            setNoteContents(item.noteContents);
-        }
+      if (item) {
+        setNoteName(item.noteName);
+        setNoteContents(item.noteContents);
+        setSelectedLabel(item.label); // 선택된 라벨 설정
+      }
     }, [item])
-
+  
     const handleUpdate = async () => {
-
-        console.log("handleUpdate")
-
-        data = {
-            noteId: item.noteId,
-            noteName: noteName,
-            noteContents: noteContents,
-            noteDate: item.noteDate,
-            noteStatus: item.noteStatus,
-            userId: item.userId,
-        }
-
-        const result = await updateNoteRequest(data);
-        console.log(result)
-
-        fetchAllNotes();
-
-        toast.show({
-            title: "수정 성공",
-            duration: 1500,
-            placement: "top",
-            avoidKeyboard: true,
-          })
-
-        navigation.navigate("NotesScreen");
+      const data = {
+        noteId: item.noteId,
+        noteName: noteName,
+        noteContents: noteContents,
+        noteDate: item.noteDate,
+        noteStatus: item.noteStatus,
+        userId: item.userId,
+        LabelsDTO: selectedLabel // 수정된 라벨 추가
+      }
+  
+      const result = await updateNoteRequest(data);
+      console.log(result);
+  
+      fetchAllNotes();
+  
+      toast.show({
+        title: "수정 성공",
+        duration: 1500,
+        placement: "top",
+        avoidKeyboard: true,
+      });
+  
+      navigation.navigate("NotesScreen");
     }
-
+  
     const handleDelete = async () => {
-      
-        console.log("handleUpdate")
-
-        data = {
-            noteId: item.noteId,
-            noteName: noteName,
-            noteContents: noteContents,
-            noteDate: item.noteDate,
-            noteStatus: item.noteStatus,
-            userId: item.userId,
-        }
-
-        const result = await deleteNoteRequest(data);
-        console.log(result)
-
-        fetchAllNotes();
-
-        toast.show({
-            title: "삭제 성공",
-            duration: 1500,
-            placement: "top",
-            avoidKeyboard: true,
-          })
-
-        navigation.navigate("NotesScreen");
-
+      const data = {
+        noteId: item.noteId,
+        noteName: noteName,
+        noteContents: noteContents,
+        noteDate: item.noteDate,
+        noteStatus: item.noteStatus,
+        userId: item.userId,
+        labelsDTO: selectedLabel // 삭제 시에도 라벨 추가
+      }
+  
+      const result = await deleteNoteRequest(data);
+      console.log(result);
+  
+      fetchAllNotes();
+  
+      toast.show({
+        title: "삭제 성공",
+        duration: 1500,
+        placement: "top",
+        avoidKeyboard: true,
+      });
+  
+      navigation.navigate("NotesScreen");
     }
-
-
+  
     return (
-        <NativeBaseProvider>
-            <Input
-                placeholder="제목을 입력해주세요"
-                value={noteName}
-                onChangeText={setNoteName}
-            />
-
-            <TextArea
-                placeholder="내용을 입력해주세요"
-                value={noteContents}
-                onChangeText={setNoteContents}
-            />
-
-            <Button
-                onPress={handleUpdate}
-            >수정</Button>
-            <Button
-                onPress={handleDelete}
-            >삭제</Button>
-        </NativeBaseProvider>
-    )
-}
-export default NoteEditorScreen;
+      <NativeBaseProvider>
+        <Input
+          placeholder="제목을 입력해주세요"
+          value={noteName}
+          onChangeText={setNoteName}
+        />
+  
+        <TextArea
+          placeholder="내용을 입력해주세요"
+          value={noteContents}
+          onChangeText={setNoteContents}
+        />
+  
+        {/* 선택된 라벨 표시 및 수정 */}
+        <Box maxW="300">
+          <Select
+            minWidth="200"
+            accessibilityLabel="Choose Service"
+            placeholder="라벨 선택"
+            value={selectedLabel} // 선택된 라벨 값 설정
+            onValueChange={(itemValue) => setSelectedLabel(itemValue)} // 선택된 라벨 값 변경 시 상태 업데이트
+          >
+            {labels.map((label) => (
+              <Select.Item key={label.labelsId} label={label.labelsTitle} value={label.labelsId} />
+            ))}
+          </Select>
+        </Box>
+  
+        <Button onPress={handleUpdate}>수정</Button>
+        <Button onPress={handleDelete}>삭제</Button>
+      </NativeBaseProvider>
+    );
+  }
+  
+  export default NoteEditorScreen;
